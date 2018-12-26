812f213644a00dc2bbef9638ce654f5d7ec093ac
Foong Kah Loun <klfoong@Foongs-MacBook-Pro-3.local>
2018-12-26 21:53:06 +0800

    asdf

---
+++ bamboo-specs/src/main/java/com/my/company/PlanSpec.java
@@ -0,0 +1,73 @@
+import com.atlassian.bamboo.specs.api.BambooSpec;
+import com.atlassian.bamboo.specs.api.builders.BambooKey;
+import com.atlassian.bamboo.specs.api.builders.BambooOid;
+import com.atlassian.bamboo.specs.api.builders.permission.PermissionType;
+import com.atlassian.bamboo.specs.api.builders.permission.Permissions;
+import com.atlassian.bamboo.specs.api.builders.permission.PlanPermissions;
+import com.atlassian.bamboo.specs.api.builders.plan.Job;
+import com.atlassian.bamboo.specs.api.builders.plan.Plan;
+import com.atlassian.bamboo.specs.api.builders.plan.PlanIdentifier;
+import com.atlassian.bamboo.specs.api.builders.plan.Stage;
+import com.atlassian.bamboo.specs.api.builders.plan.branches.BranchCleanup;
+import com.atlassian.bamboo.specs.api.builders.plan.branches.PlanBranchManagement;
+import com.atlassian.bamboo.specs.api.builders.plan.configuration.ConcurrentBuilds;
+import com.atlassian.bamboo.specs.api.builders.plan.dependencies.Dependencies;
+import com.atlassian.bamboo.specs.api.builders.plan.dependencies.DependenciesConfiguration;
+import com.atlassian.bamboo.specs.api.builders.project.Project;
+import com.atlassian.bamboo.specs.builders.task.CheckoutItem;
+import com.atlassian.bamboo.specs.builders.task.VcsCheckoutTask;
+import com.atlassian.bamboo.specs.builders.trigger.BitbucketServerTrigger;
+import com.atlassian.bamboo.specs.util.BambooServer;
+
+@BambooSpec
+public class PlanSpec {
+    
+    public Plan plan() {
+        final Plan plan = new Plan(new Project()
+                .oid(new BambooOid("11s74s5dl1nup"))
+                .key(new BambooKey("TEST"))
+                .name("TEST"),
+            "stash",
+            new BambooKey("STAS"))
+            .oid(new BambooOid("l5a4nm0q95vl"))
+            .enabled(false)
+            .pluginConfigurations(new ConcurrentBuilds()
+                    .useSystemWideDefault(false))
+            .stages(new Stage("Default Stage")
+                    .jobs(new Job("Default Job",
+                            new BambooKey("JOB1"))
+                            .tasks(new VcsCheckoutTask()
+                                    .description("Checkout Default Repository1")
+                                    .checkoutItems(new CheckoutItem().defaultRepository()))))
+            .linkedRepositories("stash-master")
+            
+            .triggers(new BitbucketServerTrigger())
+            .planBranchManagement(new PlanBranchManagement()
+                    .delete(new BranchCleanup())
+                    .notificationForCommitters())
+            .dependencies(new Dependencies()
+                    .configuration(new DependenciesConfiguration()
+                            .enabledForBranches(false)))
+            .forceStopHungBuilds();
+        return plan;
+    }
+    
+    public PlanPermissions planPermission() {
+        final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("TEST", "STAS"))
+            .permissions(new Permissions()
+                    .userPermissions("admin", PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD, PermissionType.VIEW, PermissionType.EDIT));
+        return planPermission;
+    }
+    
+    public static void main(String... argv) {
+        //By default credentials are read from the '.credentials' file.
+        BambooServer bambooServer = new BambooServer("http://localhost:8085");
+        final PlanSpec planSpec = new PlanSpec();
+        
+        final Plan plan = planSpec.plan();
+        bambooServer.publish(plan);
+        
+        final PlanPermissions planPermission = planSpec.planPermission();
+        bambooServer.publish(planPermission);
+    }
+}
